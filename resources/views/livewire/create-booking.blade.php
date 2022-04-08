5c31788b320c81fe7fc8f22977db7379463b9f8c
Sitt Aung <sittaung2021@gmail.com>
2022-04-08 11:06:31 +0630

    Adding the service time

--- resources/views/livewire/create-booking.blade.php
+++ resources/views/livewire/create-booking.blade.php
@@ -1,13 +1,11 @@
 <div class="bg-gray-200 max-w-sm mx-auto m-6 p-5 rounded-lg">
     <form>
-        {{ var_dump($state) }}
-
         <div class="mb-6">
             <label for="" class="inline-block text-gray-700 font-bold mb-2">Select service</label>
             <select name="service" id="service" class="bg-white h-10 w-full border-none rounded-lg" wire:model="state.service">
                 <option value="">Select a service</option>
                 @foreach ($services as $service)
-                    <option value="{{ $service->id }}">{{ $service->name }}</option>
+                    <option value="{{ $service->id }}">{{ $service->name }} ({{ $service->duration }} minutes)</option>
                 @endforeach
             </select>
         </div>
